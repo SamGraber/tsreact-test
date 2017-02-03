9753a634924e94d2d2e4a63d123524ebc2d36543
SamGraber <authorsamgraber@gmail.com>
2017-02-03 13:24:50 -0500

    Setup a react project with typescript and webpack

---
+++ source/hello.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react';
+
+export const Hello = (props: { compiler: string }) => <h1>Hello from {props.compiler}!</h1>;
---
+++ webpack.config.prod.js
@@ -0,0 +1,68 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var WebpackMd5Hash = require('webpack-md5-hash');
+
+module.exports = {
+	debug: true,
+	devtool: 'source-map',
+	noInfo: false,
+	resolve: {
+		// Add '.ts' and '.tsx' as resolvable extensions.
+		extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
+	},
+	entry: {
+		vendor: path.resolve(__dirname, 'source/vendor'),
+		main: path.resolve(__dirname, 'source/index'),
+	},
+	target: 'web',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/',
+		filename: '[name].[chunkhash].js',
+	},
+	plugins: [
+		// Hash the files using MD5 so that their names change when the content changes.
+		new WebpackMd5Hash(),
+
+		// Use CommonsChunkPlugin to create a separate bundle
+		// of vendor libraries so that they're cached separately.
+		new webpack.optimize.CommonsChunkPlugin({
+			name: 'vendor',
+		}),
+
+		// Create HTML file that includes reference to bundled JS.
+		new HtmlWebpackPlugin({
+			template: 'source/index.html',
+			minify: {
+				removeComments: true,
+				collapseWhitespace: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeSylteLinkTypeAttributes: true,
+				keepClosingSlash: true,
+				minifyJS: true,
+				minifyCSS: true,
+				minifyURLs: true,
+			},
+			inject: true,
+			production: true,
+		}),
+
+		// Eliminate duplicate packages when generating bundle
+		new webpack.optimize.DedupePlugin(),
+
+		// Minify JS
+		new webpack.optimize.UglifyJsPlugin(),
+	],
+	module: {
+		loaders: [
+			{ test: /\.tsx?$/, exclude: /node_modules/, loaders: ['awesome-typescript-loader'] },
+			{ test: /\.css$/, loaders: ['style', 'css'] },
+		],
+		preLoaders: [
+			{ test: /\.js$/, loader: 'source-map-loader' },
+		],
+	},
+};
